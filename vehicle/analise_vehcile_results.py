d451e2c0c65b42541696730bca636116c2136d7a
mwojtowi <01178261@pw.edu.pl>
2025-06-05 10:02:51 +0200

    vehicles

--- vehicle/analise_vehcile_results.py
+++ vehicle/analise_vehcile_results.py
@@ -1,4 +1,13 @@
+import sys
+import os
+#
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from plot_utils import generate_full_report
+#
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+## 
+RESULTS_DIR = os.path.join(BASE_DIR, 'results')
+
 
 generate_full_report("trees_number", 1, y_range=(0.1, 1.0))
 generate_full_report("epsilon", 1, y_range=(0.1, 1.0))
